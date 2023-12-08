3b81da9297436150c5368faf363dbae0e9446b59
Vijayaraj-Anbalagan <Vijayaraj.Anbalagan@gmail.com>
2023-12-08 14:23:46 +0530

    first commit

--- components/intro.tsx
+++ components/intro.tsx
@@ -90,7 +90,7 @@ export default function Intro() {
 
         <a
           className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:hover:text-white/100"
-          href="/CV.pdf"
+          href="https://drive.google.com/file/d/1hovu4b8p55z4q_oc5CeXHKBUgK5YvSk3/view?usp=sharing"
           download
         >
           Download CV{" "}
